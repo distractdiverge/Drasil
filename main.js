27d84e57f81602fdb76a5649d6904c70537f104f
Alex Lapinski <alexander.lapinski@razorfishhealth.com>
2013-02-11 13:00:15 -0500

    added router

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 var n3 = require("n3");
 var http = require("http");
+var router = require("router");
+var route = router();
 var server;
+var store;
 
 
 // Generate Dummy Content in n3 store
-var store = new n3.Store();
-store.add(":Pluto", "a", ":Dog");
-store.add(":Mickey", "a", ":Mouse");
+store = new n3.Store();
+store.add(":pluto", "a", ":Dog");
+store.add(":mickey", "a", ":Mouse");
 console.log("Loaded test data");
 
-server = http.createServer(function(req, res){
+route.get("/",function(req,res){
+   res.writeHead(200, {"Content-Type":"text/html"});
+   res.end("<html><head><title>Drasil</title></head><body>Drasil Index Page<br><a href=\"/mickey\">Mickey</a></body></html>");
+});
 
-	console.log("Processing request " + req);
+route.get("/{subject}", function(req, res) {
 
-   if(req.path == "/mickey") 
-   {
+   var result = store.find(":"+req.params.subject, null, null)[0];
 
-   	var mickey = store.find(":Mickey", null, null)[0];
-   	res.writeHead(200, {"Content-Type":"text/n3"});
-   	res.end(mickey.subject, mickey.predicate, mickey.object, ".");
+   if( !result ) {
+      res.writeHead(404);
+      res.end();
+      return;
+   }
+
+   res.writeHead(200, {"Content-Type":"text/n3"});
    
-   }
-   else 
-   {
-   	res.writeHead(404);
-   	res.end();
-   }
-
-}).listen(1337, "127.0.0.1");
+   res.end(""+result.subject+" "+result.predicate+" "+result.object+" .");
+});
+   
+server = http.createServer(route).listen(1337);
 console.log("Server Running at http://127.0.0.1:1337");
