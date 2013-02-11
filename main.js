c3d0ead2bf664e4313753767d885f7a3e90b838a
Alex Lapinski <contact@alexlapinski.name>
2013-02-10 20:11:47 -0500

    Added n3 reference

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-var console = require("console");
-
-console.log("test");
-
+var n3 = require("n3");
 var http = require("http");
-http.createServer(function(req, res){
-   res.writeHead(200, {"Content-Type":"text/plain"});
-   res.end("Hello World\n");
+var server;
+
+
+// Generate Dummy Content in n3 store
+var store = new n3.Store();
+store.add(":Pluto", "a", ":Dog");
+store.add(":Mickey", "a", ":Mouse");
+console.log("Loaded test data");
+
+server = http.createServer(function(req, res){
+
+	console.log("Processing request " + req);
+
+   if(req.path == "/mickey") 
+   {
+
+   	var mickey = store.find(":Mickey", null, null)[0];
+   	res.writeHead(200, {"Content-Type":"text/n3"});
+   	res.end(mickey.subject, mickey.predicate, mickey.object, ".");
+   
+   }
+   else 
+   {
+   	res.writeHead(404);
+   	res.end();
+   }
+
 }).listen(1337, "127.0.0.1");
 console.log("Server Running at http://127.0.0.1:1337");
